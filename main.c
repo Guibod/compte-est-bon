96d4989cd3888c3955c2c018964d4f56149cf915
Guillaume Boddaert <gboddaert@solocal.com>
2022-12-26 00:15:14 +0100

    feat: working command line arguments

--- main.c
+++ main.c
@@ -1,20 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 int resolution(int *sample, int nb, int target, char **operations);
-int *cloneIntArray(int const *src, size_t len);
+int* cloneIntArray(int const *src, size_t len);
 int parseFile(char* fname, int *nb, int *target, int *sample);
+int* randomSample(int nb);
+void printProblem(int nb, int target, int* sample);
 
-int main() {
+int main(int argc, char *argv[]) {
   int nb, target;
   int* sample = NULL;
   sample = malloc(sizeof(int));
 
-  int ret = parseFile("/Users/gboddaert/CLionProjects/compte-est-bon/files/jeu6-1.txt", &nb, &target, sample);
-  if (ret != 0) {
-    return ret;
+  if( argc == 3 ) {
+    nb = atoi(argv[1]);
+    target = atoi(argv[2]);
+    sample = randomSample(nb);
   }
+  else if (argc == 2) {
+    int ret = parseFile(argv[1], &nb, &target, sample);
+    if (ret != 0) {
+      fprintf(stderr, "Failed to parse %s", argv[1]);
+      return 1;
+    }
+  } else {
+    printf("Combien de cartes ?");
+    fscanf (stdin, "%d", &nb);
+    printf("Quelle valeur cible ?");
+    fscanf (stdin, "%d", &target);
+    sample = randomSample(nb);
+  };
 
   char **operations = malloc((nb + 4) * sizeof *operations);
   int i;
@@ -26,16 +43,17 @@ int main() {
   operations[2] = "/";
   operations[3] = "*";
 
+  printProblem(nb, target, sample);
   int res = resolution(sample, nb, target, operations);
   if (res == 0) {
-    printf("A solution was found !\n");
+    printf("Une solution au problème a été trouvée !\n");
     for (i = nb + 4 - 1; i > 4; --i) {
       printf("%s\n", operations[i]);
     }
     return 0;
   }
 
-  printf("No solution was found!\n");
+  printf("Aucune solution trouvée au problème!\n");
   return 1;
 }
 
@@ -115,8 +133,7 @@ int *cloneIntArray(int const *src, size_t len) {
 }
 
 int parseFile(char* fname, int *nb, int *target, int *sample) {
-  if(fname == NULL || nb == NULL || target == NULL
-      || sample == NULL) {
+  if(fname == NULL || nb == NULL || target == NULL || sample == NULL) {
     return -1;
   }
 
@@ -147,4 +164,36 @@ int parseFile(char* fname, int *nb, int *target, int *sample) {
   }
   fclose(fp);
   return 0;
+}
+
+int* randomSample(int nb) {
+  int i, j;
+  int* sample = NULL;
+  sample = malloc(sizeof(int));
+  int items[24] = {
+    1, 1, 2, 2, 3, 3,
+    4, 4, 5, 5, 6, 6,
+    7, 7, 8, 8, 9, 9,
+    10, 10, 25, 50, 75, 100
+  };
+
+  srand(time(NULL));
+  for (i=0; i<nb; ++i) {
+    j = rand() % (24 - i);
+    sample[i] = items[j];
+    items[j] = items[23-i];
+  }
+
+  return sample;
+}
+
+void printProblem(int nb, int target, int* sample) {
+  int i;
+  printf("==============================\n");
+  printf("Valeur à calculer: %d\n", target);
+  printf("En utilisant %d valeurs: \n", nb);
+  for (i=0; i<nb; i++) {
+    printf("  - %d\n", sample[i]);
+  }
+  printf("==============================\n\n");
 }
