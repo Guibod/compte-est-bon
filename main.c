f481ef103105761f5d9fe6e78f5f6982002cca44
Guillaume Boddaert <gboddaert@solocal.com>
2022-12-25 22:36:25 +0100

    some light refactoring

--- main.c
+++ main.c
@@ -1,109 +1,110 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
-int resolution(int* tirage, int nb, int cible, char** operations);
-int * intdup(int const * src, size_t len);
 
+int resolution(int *sample, int nb, int target, char **operations);
+int *cloneIntArray(int const *src, size_t len);
 
 int main() {
-    int nb = 6;
-    int tirage[6] = {10, 1, 25, 9, 3, 6};
-    int cible = 595;
-//    int nb = 3;
-//    int tirage[3] = {10, 1, 2};
-//    int cible = 8;
-//    int nb = 2;
-//    int tirage[2] = {10, 2};
-//    int cible = 20;
+  int nb = 6;
+  int sample[6] = {10, 1, 25, 9, 3, 6};
+  int target = 595;
 
-    char **operations = malloc((nb + 4) * sizeof *operations);
-    int i;
-    if (operations)
-        for (i = 0; i < (nb+4); i++)
-            operations[i] = malloc(256 * sizeof *operations[i]);
-    operations[0] = "+";
-    operations[1] = "-";
-    operations[2] = "/";
-    operations[3] = "*";
+  char **operations = malloc((nb + 4) * sizeof *operations);
+  int i;
+  if (operations)
+    for (i = 0; i < (nb + 4); i++)
+      operations[i] = malloc(256 * sizeof *operations[i]);
+  operations[0] = "+";
+  operations[1] = "-";
+  operations[2] = "/";
+  operations[3] = "*";
 
-    int res = resolution(tirage, nb, cible, operations);
-    for (i=nb + 4 - 1; i > 4; --i) {
-        printf("%s\n", operations[i]);
+  int res = resolution(sample, nb, target, operations);
+  if (res == 0) {
+    printf("A solution was found !\n");
+    for (i = nb + 4 - 1; i > 4; --i) {
+      printf("%s\n", operations[i]);
     }
     return 0;
+  }
+
+  printf("No solution was found!\n");
+  return 1;
 }
 
-int resolution(int* tirage, int nb, int cible, char** operations) {
-    int i, j, k;
-    // tout les nombres tirage[i] Faire
-    for (i=0; i < nb; ++i) {
-        // Pour tout les nombres tirage[j] Faire
-        for (j=0; j < nb; ++j) {
-            // avec i! = j
-            if (i == j) {
-                continue;
-            }
+/**
+ * @param sample
+ * @param nb
+ * @param target
+ * @param operations
+ * @return int
+ */
+int resolution(int *sample, int nb, int target, char **operations) {
+  int i, j, k;
+  // tout les nombres sample[i] Faire
+  for (i = 0; i < nb; ++i) {
+    // Pour tout les nombres sample[j] Faire
+    for (j = 0; j < nb; ++j) {
+      // avec i! = j
+      if (i == j) {
+        continue;
+      }
 
-            // Pour tout les 4 op´erations
-            for (k=0; k < 4; ++k) {
-                // Sauvegarder l’´etat du tableau tirage
-                int* backup = intdup(tirage, nb);
+      // Pour tout les 4 op´erations
+      for (k = 0; k < 4; ++k) {
+        // Sauvegarder l’´etat du tableau sample
+        int *backup = cloneIntArray(sample, nb);
 
-                int res;
-                char op = operations[k][0];
-                int val1 = tirage[i];
-                int val2 = tirage[j];
+        int res = -1;
+        char op = operations[k][0];
+        int val1 = sample[i];
+        int val2 = sample[j];
 
-                // Effectuer le calcul tirage[i] operation tirage[j]
-                if (strcmp(operations[k],"+") == 0) {
-                    res = tirage[i] + tirage[j];
-                } else if (strcmp(operations[k],"-") == 0) {
-                    res = tirage[i] - tirage[j];
-                } else if (strcmp(operations[k],"/") == 0) {
-                    res = tirage[i] / tirage[j];
-                } else if (strcmp(operations[k],"*") == 0) {
-                    res = tirage[i] * tirage[j];
-                }
-
-                // Mettre le r´esultat dans tirage[i]
-                tirage[i] = res;
-
-                // Mettre le dernier nombre non utilis´e tirage[nb − 1] `a la place de tirage[j]
-                tirage[j] = tirage[nb - 1];
-
-                // Si le r´esultat est la cible OU resolution(tirage, nb-1, cible, operations) == 0 Alors
-                if (res == cible || resolution(tirage, nb-1, cible, operations) == 0) {
-                    tirage = backup;
-                    sprintf(operations[nb + 4 - 1], "%d %c %d = %d", val1, op, val2, res);
-                    return 0;
-                }
-
-                tirage = backup;
-            }
+        // Effectuer le calcul sample[i] operation sample[j]
+        if (strcmp(operations[k], "+") == 0) {
+          res = sample[i] + sample[j];
+        } else if (strcmp(operations[k], "-") == 0) {
+          res = sample[i] - sample[j];
+        } else if (strcmp(operations[k], "/") == 0) {
+          res = sample[i] / sample[j];
+        } else if (strcmp(operations[k], "*") == 0) {
+          res = sample[i] * sample[j];
         }
-        return 1;
+
+        // Mettre le r´esultat dans sample[i]
+        sample[i] = res;
+
+        // Mettre le dernier nombre non utilis´e sample[nb − 1] `a la place de
+        // sample[j]
+        sample[j] = sample[nb - 1];
+
+        // Si le r´esultat est la target OU resolution(sample, nb-1, target,
+        // operations) == 0 Alors
+        if (res == target ||
+            resolution(sample, nb - 1, target, operations) == 0) {
+          sample = backup;
+          sprintf(operations[nb + 4 - 1], "%d %c %d = %d", val1, op, val2, res);
+          return 0;
+        }
+
+        sample = backup;
+      }
     }
-//    10: //On a trouv´e une solution, soit imm´ediatement (resultat=cible)
-//    11: //soit en continuant les op´erations avec les nb-1 nombres restants
-//    12: //On met l’operation et les nombres dans le tableau operations
-//    13: Mettre les nombres, l’op´eration utilis´es et le r´esultat dans le tableau operations
-//    14: Restaurer les valeurs du tableau tirage
-//    15: return 0 ;
-//    16: Fin Si
-//    17: // tirage[i] operation tirage[j] ne mˆene pas `a une solution.
-//    18: // On essaye une autre op´eration ou un autre couple i,j
-//    19: Restaurer les valeurs du tableau tirage
-//    20: Fin Pour
-//    21: Fin Pour
-//    22: Fin Pour
-//    23: //Aucune solution possible si on arrive ici
-//    24: return 1 ;
+  }
+  return 1;
 }
 
-int * intdup(int const * src, size_t len)
-{
-    int * p = malloc(len * sizeof(int));
-    memcpy(p, src, len * sizeof(int));
-    return p;
+/**
+ * clone an integer array
+ *
+ * @param src
+ * @param len
+ * @return
+ */
+int *cloneIntArray(int const *src, size_t len) {
+  int *p = malloc(len * sizeof(int));
+  memcpy(p, src, len * sizeof(int));
+  return p;
 }
