1917121f8c75320b6d4a6446e8862e0dc23b49cf
Guillaume Boddaert <gboddaert@solocal.com>
2022-12-25 16:28:00 +0100

    figured out how to declare variable, yeah

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+int resolution(int* tirage, int nb, int cible, char** operations) {
+    return 1;
+}
+
 int main() {
-    printf("Hello, World!\n");
+    int nb = 6;
+    int tirage[6] = {10, 1, 25, 9, 3, 6};
+    int cible = 595;
+    char *operations[4] = {"+", "-", "/", "*"};
+
+    int res = resolution(tirage, nb, cible, operations);
+    printf("%d", res);
     return 0;
 }
+
